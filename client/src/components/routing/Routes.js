f2d3f499eae5616c03bc85d7d637423f8e88f0c3
Damian <damian20lewandowski@gmail.com>
2019-07-09 14:36:14 +0200

    Removed commented out code

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -36,7 +36,6 @@ const Routes = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          {/* <Route exact path="/profile" component={Profile} /> */}
           <ProtectedRoute exact path="/profile" component={Profile} />
           <ProtectedRoute exact path="/profile/edit" component={EditProfile} />
           <Route component={NotFound} />
